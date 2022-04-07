a63515bebaff9e6ff03d6bb7f2c04d3d609d5387
Ernane Sousa <ernanegeraldo@hotmail.com>
2022-04-07 17:21:52 -0300

    Finalização desafio 12

--- financys/src/test/java/trilha/back/financys/adapters/inbound/http/Entry/TrilhaBackTestesController.java
+++ financys/src/test/java/trilha/back/financys/adapters/inbound/http/Entry/TrilhaBackTestesController.java
@@ -8,10 +8,15 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import trilha.back.financys.adapters.exceptions.ListaVaziaExceptions;
 import trilha.back.financys.adapters.inbound.http.Entry.mappers.EntryMapper;
+import trilha.back.financys.core.domains.Category;
 import trilha.back.financys.core.domains.Entry;
 import trilha.back.financys.core.exceptions.EntryNotFoundException;
 import trilha.back.financys.core.services.CategoryService;
 import trilha.back.financys.core.services.EntryService;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
@@ -57,12 +62,25 @@ public class TrilhaBackTestesController {
     @Test
     @DisplayName("Deve ter sucesso ao filtrar lançamentos")
     public void deveRetornarFiltroDeLancamentos() throws Exception {
+
+        String date = "05042022";
+        Double amount= 100.0;
+        Boolean paid = true;
+
+        Category category = new Category(1l, "Salario", "Pagamento de salário");
+
+        List<Entry> entries = new ArrayList<>();
+        entries.add(new Entry(1L, "Pagamento de salário", "Pagamento", "Despesa", 1500.0, "05032022", false, category));
+        entries.add(new Entry(2L, "Pagamento de salário", "Pagamento", "Despesa", amount, date, paid, category));
+
+        when(entryService.lancamentosDependentes("05042022", 100.0, true)).thenReturn(entries);
         this.mockMvc.perform(get("/entry/filter")
                         .param("date", "05042022")
-                        .param("amount", "10.0")
+                        .param("amount", "100.0")
                         .param("paid", "true")
                         .contentType("Application/Json"))
                 .andExpect(status().isOk());
+
     }
 
     @Test
